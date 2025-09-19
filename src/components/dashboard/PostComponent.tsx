113b724218e8b17f500ab372a43a87bca1eea855
Ángel Uriel González Urrutia <115201033+UriProtocol@users.noreply.github.com>
2025-09-19 01:07:39 -0600

    show date

--- src/components/dashboard/PostComponent.tsx
+++ src/components/dashboard/PostComponent.tsx
@@ -2,7 +2,7 @@ import { Post } from "@/lib/types";
 import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 import Image from "next/image";
 
-export default function PostComponent({ post, user_uuid }: { post: Post , user_uuid: string}) {
+export default function PostComponent({ post, user_uuid }: { post: Post, user_uuid: string }) {
     return (
         <Card className=" w-full border-2 border-slate-400/50 text-regal-blue" shadow="none">
             <CardHeader className="flex gap-3">
@@ -13,9 +13,12 @@ export default function PostComponent({ post, user_uuid }: { post: Post , user_u
                     width={40}
                     className=" rounded-full"
                 />
-                <div className="flex flex-col">
-                    <p className="text-md">{post.user_uuid == user_uuid ? 'Yo': post.creator?.name}</p>
-                    <p className="text-small text-default-500 opacity-70">{post.user_uuid == user_uuid ? '': post.creator?.email}</p>
+                <div className="flex flex-col w-full">
+                    <p className="text-md">{post.user_uuid == user_uuid ? 'Yo' : post.creator?.name}</p>
+                    <div className=" flex justify-between w-full">
+                        <p className="text-small text-default-500 opacity-70">{post.user_uuid == user_uuid ? '' : post.creator?.email}</p>
+                        <p className="text-small text-default-500 opacity-50">{new Date(post.created_at).toLocaleString()}</p>
+                    </div>
                 </div>
             </CardHeader>
             <Divider />
@@ -23,7 +26,7 @@ export default function PostComponent({ post, user_uuid }: { post: Post , user_u
                 <p className="font-semibold">{post.name}</p>
                 <p className="font-normal opacity-60">{post.description}</p>
             </CardBody>
-            <Divider />
+
         </Card>
     )
 }
